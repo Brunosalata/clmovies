6f0f9b32f954ae54d19ba5f33e1d903e241cad86
Bruno Salata <brunoslima@gmail.com>
2022-05-11 19:15:58 -0300

    Projeto criado

---
+++ backend/src/main/java/com/brunosalata/clmovies/ClmoviesApplication.java
@@ -0,0 +1,13 @@
+package com.brunosalata.clmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClmoviesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/brunosalata/clmovies/ClmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunosalata.clmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
